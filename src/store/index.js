d45b790cb4e14841db4af8e1c0a320fe0b5c80da
liuchang <497123430@qq.com>
2020-11-12 21:29:16 +0800

    123

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+
+import menu from './modules/menu'
+import role from './modules/role'
+import manger from './modules/manger'
+import classify from './modules/classify'
+import spec from './modules/spec'
+export default new Vuex.Store({
+        modules:{
+            menu,
+            role,
+            manger,
+            classify,
+            spec
+        }
+})
---
+++ src/util/alert.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+let vm = new Vue();
+
+
+export const alertSuccess=(msg)=>{
+    vm.$message({
+        message: msg,
+        type: 'success'
+      });
+}
+
+export const alertwarning=(msg)=>{
+    vm.$message({
+        message: msg,
+        type: 'warning'
+      });
+}
+
